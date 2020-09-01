def1e412a7aa728a6a24a65692d6a15174238809
Bokuto14 <70587327+Bokuto14@users.noreply.github.com>
2020-09-01 19:53:07 +0530

    created abc.py
    task 1

---
+++ abc.py
@@ -0,0 +1,11 @@
+a=float(input("Input the radius of the circle: "))
+b=3.141592653589793238*(a**2)
+print("The area of the circle with radois a is :", b)
+
+
+
+c=input("Input the Filename: ")
+d="py"
+for i in (c.split(".")):
+    if i == d:
+        print("The extension of the file is: Python") 
